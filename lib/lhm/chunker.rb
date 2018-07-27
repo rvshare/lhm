c513c31d4df1d987771a12524b22d0c5603f4d96
Brian Buchalter <brian.buchalter@shopify.com>
2018-07-27 09:30:18 -0600

    Fix ChunkFinder to work with empty tables
    When there is an empty table, there will be no start or limit
    found. This is acceptable behavior and is supported by Chunker,
    but was not when ChunkFinder was extracted.

--- lib/lhm/chunker.rb
+++ lib/lhm/chunker.rb
@@ -64,32 +64,32 @@ module Lhm
   end
 
   class ChunkFinder
-    attr_accessor :start, :limit
-
     def initialize(migration, connection = nil, options = {})
       @migration = migration
       @connection = connection
       @router = Router.new(migration)
-      @start = options[:start] || select_start
-      @limit = options[:limit] || select_limit
+      @start = options[:start] || select_start_from_db
+      @limit = options[:limit] || select_limit_from_db
     end
 
+    attr_accessor :start, :limit
+
     def validate
-      if @start > @limit
-        raise ArgumentErrorerror, "impossible chunk options (limit (#{@limit.inspect} must be greater than start (#{@start.inspect})"
+      # We only validate if we have a start and a limit.
+      # The absence of a start and a limit imply an empty table.
+      if start && limit && start > limit
+        raise ArgumentError, "impossible chunk options (limit (#{limit.inspect} must be greater than start (#{start.inspect})"
       end
     end
 
     private
 
-    def select_start
-      start = @connection.select_value("select min(id) from `#{ @router.origin_name }`")
-      start ? start.to_i : nil
+    def select_start_from_db
+      @connection.select_value("select min(id) from `#{ @router.origin_name }`")
     end
 
-    def select_limit
-      limit = @connection.select_value("select max(id) from `#{ @router.origin_name }`")
-      limit ? limit.to_i : nil
+    def select_limit_from_db
+      @connection.select_value("select max(id) from `#{ @router.origin_name }`")
     end
   end
 
